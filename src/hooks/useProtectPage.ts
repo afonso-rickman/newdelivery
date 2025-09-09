8a57b2a028d0ed9decade2cab38b5fe7ef248039
Douglas CodClick <appdeliveryfacil@gmail.com>
2025-09-09 11:53:23 -0300

    testando useProtectPage.ts adicionada linha 5 by Douglas

--- src/hooks/useProtectPage.ts
+++ src/hooks/useProtectPage.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/contexts/AuthContext";
 import { supabase } from "@/lib/supabaseClient";
+import { useAuth } from "@/hooks/useAuth";
 
 export function useProtectPage(expectedRole: string) {
   const { currentUser } = useAuth();
