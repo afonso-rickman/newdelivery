535bd6bcc83af59b722b5b3deedd590e5b3c0ed3
Douglas CodClick <appdeliveryfacil@gmail.com>
2025-09-09 21:23:23 -0300

    trocado logOut por signOut para resolver problema de logout - Login está funcionando ok

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -2,13 +2,13 @@ import { useAuth } from "@/hooks/useAuth";
 import { Button } from "@/components/ui/button";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
-  const { currentUser, logOut } = useAuth();
+  const { currentUser, signOut } = useAuth();
 
   return (
     <div>
       {currentUser && (
         <div className="fixed top-4 right-4 z-50">
-          <Button onClick={logOut} variant="outline">
+          <Button onClick={signOut} variant="outline">
             Sair
           </Button>
         </div>
